26170b2b03a85d813ab05135365f74e17a1e3045
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-12-15 12:02:54 +0700

    update: button masuk

--- app/src/main/java/com/example/galonhub/LoginActivity.kt
+++ app/src/main/java/com/example/galonhub/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.galonhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val tombol_login = findViewById<Button>(R.id.button2)
+        tombol_login.setOnClickListener{
+            val halamanBaru = Intent(this@LoginActivity, HomeActivity::class.java)
+            halamanBaru.putExtra("login", true)
+            startActivity(halamanBaru)
+        }
+
+        val tombol_register = findViewById<TextView>(R.id.textView18)
+        tombol_register.setOnClickListener{
+            val halamanBaru = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(halamanBaru)
+        }
     }
 }
