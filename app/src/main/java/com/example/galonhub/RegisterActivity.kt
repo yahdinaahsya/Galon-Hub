95f3ee23cf569295e0f9dcd810ecd9181b71e8af
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-12-15 15:10:36 +0700

    update

--- app/src/main/java/com/example/galonhub/RegisterActivity.kt
+++ app/src/main/java/com/example/galonhub/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,12 @@ class RegisterActivity : AppCompatActivity() {
         tombol.setOnClickListener{
             val halamanBaru = Intent(this@RegisterActivity, LoginActivity::class.java)
             halamanBaru.putExtra("login", true)
+            startActivity(halamanBaru)
+        }
+
+        val tombol_login = findViewById<TextView>(R.id.textView18)
+        tombol_login.setOnClickListener{
+            val halamanBaru = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(halamanBaru)
         }
     }
