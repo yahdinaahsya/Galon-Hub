548d6f97d377346c68de42e745ccd55ce39fe0c3
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-12-15 11:19:56 +0700

    update: konekkan ikon profil ke profilpage

--- app/src/main/java/com/example/galonhub/HomeActivity.kt
+++ app/src/main/java/com/example/galonhub/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.galonhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
+
+        val tombol = findViewById<ImageView>(R.id.imageView9)
+        tombol.setOnClickListener {
+            val halamanBaru = Intent(this@HomeActivity, ProfilActivity::class.java)
+            startActivity(halamanBaru)
+        }
     }
 }
