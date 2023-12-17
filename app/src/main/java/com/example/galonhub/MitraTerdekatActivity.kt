48363b4ef14e05bd4401e839256299641dc5bc56
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-12-17 20:18:55 +0700

    login dan registrasi

--- app/src/main/java/com/example/galonhub/MitraTerdekatActivity.kt
+++ app/src/main/java/com/example/galonhub/MitraTerdekatActivity.kt
@@ -1,11 +1,20 @@
 package com.example.galonhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MitraTerdekatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mitra_terdekat)
+
+        val kembali = findViewById<Button>(R.id.button6)
+        kembali.setOnClickListener{
+            val halamanBaru = Intent(this@MitraTerdekatActivity, HomeActivity::class.java)
+            startActivity(halamanBaru)
+            finish()
+        }
     }
 }
