ea5b0a2f2e4c1732e2e344c30c58cb1650c113d4
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-12-19 06:49:50 +0700

    fix layout wawasan ke insight

--- app/src/main/java/com/example/galonhub/WawasanActivity.kt
+++ app/src/main/java/com/example/galonhub/WawasanActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class WawasanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.wawasan)
+        setContentView(R.layout.insight)
     }
 }
