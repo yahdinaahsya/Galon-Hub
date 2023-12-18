1e546e5a551b72f4fdcfb7c2ecab563ba5ebfefb
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-12-18 23:11:17 +0700

    tombol ke detail pesanan

--- app/src/main/java/com/example/galonhub/MitraTerdekatActivity.kt
+++ app/src/main/java/com/example/galonhub/MitraTerdekatActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MitraTerdekatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,13 @@ class MitraTerdekatActivity : AppCompatActivity() {
         val kembali = findViewById<Button>(R.id.button6)
         kembali.setOnClickListener{
             val halamanBaru = Intent(this@MitraTerdekatActivity, HomeActivity::class.java)
+            startActivity(halamanBaru)
+            finish()
+        }
+
+        val toko = findViewById<TextView>(R.id.textView85)
+        toko.setOnClickListener {
+            val halamanBaru = Intent( this@MitraTerdekatActivity, DetailPesananActivity::class.java)
             startActivity(halamanBaru)
             finish()
         }
